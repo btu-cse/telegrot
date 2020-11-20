22dc8e9fd1d7f685f4904238757e2a3f2e3c4e73
Fatih Ateş <51250249+fatiiates@users.noreply.github.com>
2020-11-21 00:35:32 +0300

    0.1.0
    Signed-off-by: Fatih ATES <fatiiates@gmail.com>

--- replica.py
+++ replica.py
@@ -215,11 +215,7 @@ def main():
 
     # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
+    
 if __name__ == '__main__':
     main()
