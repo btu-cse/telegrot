c4e9e1b89d3a9c6d3435c64895a179bb20ad5f2c
Emine Sener <eminesener063@gmail.com>
2023-03-03 21:30:07 +0300

    Updated scraper method
    Signed-off-by: Fatih ATES <fatiiates@gmail.com>

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,63 +1,81 @@
 import requests
 from bs4 import BeautifulSoup
 from src.common.logger import Logger
- 
+
 
 class Scraper:
-    site_url: str = "https://mdbf.btu.edu.tr/bilgisayar"
+    site_url: str = "https://mdbf.btu.edu.tr/tr/bilgisayar/duyuru/birim/193"
 
-    # gets announcement id: developed for getting last announcement and serializing it
-    # if announcement = 0 then gets the last announcement id
-    # if announcement = 1 then gets the last-1 announcement id
     @staticmethod
-    def get_announcement_id(announcement: int) -> int:
+    def get_announcement_id(announcement: int) -> str:
         try:
-            params = {'page': 'duyuru'}
-
-            page = requests.get(Scraper.site_url, params=params, verify=False)
+            page = requests.get(Scraper.site_url)
             soup = BeautifulSoup(page.content, 'html.parser')
-            container = soup.find_all("div", {"class": "container"})[1]
-            row = container.find_all("div", {"class": "row"})[0]
-            column = row.find_all("div", {"class": "col-md-9"})[0]
+            ann_list= soup.find_all('div',class_ = "ann-list")[0].find_all('ul')[0]
+            anns = ann_list.find_all('li')
 
-            i = 0
-            for val in column.find_all('table'):
-                if i == announcement:
-                    return int(val.find_all('a')[0].get('href').split('&')[1].split('=')[1])
-                i += 1
+            
+            i=0
+            for val in anns:
+                if i==announcement:
+                    return val.find_all('a')[0].get('href').split('/')[7]
+                i+=1
+                
 
         except Exception as e:
             Logger.error("there is an error while getting announcement id from website, Announcement number: {0} \n".format(
                 announcement), e)
 
-        return -1
-
+        return ""
+    
     @staticmethod
-    def get_last_announcement_id() -> int:
+    def get_last_announcement_id() -> str:
         return Scraper.get_announcement_id(0)
-
+    
     # gets last announcement's content: developed for sent the last announcement as a content
     @staticmethod
-    def get_announcement_content_by_id(announcement: int) -> str:
+    def get_announcement_content_by_id(announcement: str) -> str:
         try:
-            params = {'page': 'duyuru', 'id': announcement}
+            
 
-            page = requests.get(Scraper.site_url, params=params, verify=False)
+            page = requests.get(Scraper.site_url)
             soup = BeautifulSoup(page.content, 'html.parser')
-            container = soup.find_all("div", {"class": "container"})[1]
-            row = container.find_all("div", {"class": "row"})[0]
-            column = row.find_all("div", {"class": "col-md-9"})[0]
+            ann_list= soup.find_all('div',class_ = "ann-list")[0].find_all('ul')[0]
+            anns = ann_list.find_all('li')
+        
+            ann=""
+        
+        
+            for val in anns:
+                if announcement in val.find_all('a')[0].get('href').split('/')[7]:
+                    ann_id=val.find_all('a')[0].get('href')
+            
+        
+            ann_page=requests.get(ann_id, verify=False)
+            soup = BeautifulSoup(ann_page.content, 'html.parser')
+            ann_text= soup.find_all('div',class_ = "page-about")[0].get_text()
+            ann_text += '\nİncelemek için: ' + str(ann_id) + '?'
+                        
+            return ann_text
 
-            panel = column.find_all("div", {"class": "panel"})[0]
-            panel_body = panel.find_all('div')[1].get_text()
-            panel_body += '\nİncelemek için: ' + str(Scraper.site_url) + '?'
-
-            for val in params.keys():
-                panel_body += str(val) + '=' + str(params[val]) + '&'
-
-            return panel_body.rstrip('&')
         except Exception as e:
             Logger.error("there is an error while getting announcement's content from website, Announcement number: {0}".format(
-                announcement), e)
+             announcement), e)
 
-        return ""
+            return ""
+
+
+                
+            
+
+                
+            
+            
+            
+            
+            
+
+
+            
+            
+
